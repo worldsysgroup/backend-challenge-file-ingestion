c9051a14fab99215a4393df917b0e57998d8936e
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-07 23:04:52 -0300

    feat: add logger instance

---
+++ node-api/src/utils/logger.instance.ts
@@ -0,0 +1,28 @@
+import { env } from "@app/config/env.config";
+import pino, { LoggerOptions } from "pino";
+
+const isProduction = env.APP_ENV === "production";
+const level = env.LOG_LEVEL || (isProduction ? "info" : "debug");
+
+const options: LoggerOptions = {
+  level,
+  redact: ['req.headers.authorization', 'user.password'],
+  serializers: {
+    err: pino.stdSerializers.err,
+    req: (req) => ({ id: req.id, method: req.method, url: req.url }),
+  },
+  ...(isProduction
+    ? {}
+    : {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            ignore: "pid,hostname",
+            translateTime: "SYS:standard",
+          },
+        },
+      }),
+};
+
+export const logger = pino(options);
